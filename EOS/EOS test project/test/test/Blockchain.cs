141aef63c65894383589afc37693deb3d0cfce24
grogn <3ykovanton@gmail.com>
2018-04-21 10:11:02 +0500

    Blockchain refactoring

--- EOS/EOS test project/test/test/Blockchain.cs
+++ EOS/EOS test project/test/test/Blockchain.cs
@@ -15,37 +15,48 @@ namespace test
 			serverAddress = "127.0.0.1:8888";
 		}
 
+		public static string GetRequest(string methodName)
+		{
+			var request = (HttpWebRequest)HttpWebRequest.Create($"http://{serverAddress}/v1/chain/{methodName}");
+            request.Method = "GET";
+            string responseData;
+            using (var response = (HttpWebResponse)request.GetResponse())
+            using (var dataStream = response.GetResponseStream())
+            using (var reader = new StreamReader(dataStream))
+                responseData = reader.ReadToEnd();
+            return responseData;
+		}
+
+		public static string PostRequest(string methodName, string postData)
+		{
+			var request = (HttpWebRequest)HttpWebRequest.Create($"http://{serverAddress}/v1/chain/{methodName}");
+            var data = Encoding.ASCII.GetBytes(postData);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            string responseData;
+            using (var response = (HttpWebResponse)request.GetResponse())
+            using (var dataStream = response.GetResponseStream())
+            using (var reader = new StreamReader(dataStream))
+                responseData = reader.ReadToEnd();
+            return responseData;
+		}
+
 		public static string GetInfo()
 		{
-			var request = (HttpWebRequest)HttpWebRequest.Create($"http://{serverAddress}/v1/chain/get_info");
-			request.Method = "GET";
-			string responseData;
-			using (var response = (HttpWebResponse)request.GetResponse())
-				using (var dataStream = response.GetResponseStream())
-					using (var reader = new StreamReader(dataStream))
-						responseData = reader.ReadToEnd();
-			return responseData;
+			return GetRequest("get_info");
 		}
 
 		public static string GetAccount(string accountName)
 		{
-			var request = (HttpWebRequest)HttpWebRequest.Create($"http://{serverAddress}/v1/chain/get_account");
 			var postData = $"{{\"account_name\":\"{accountName}\"}}";
-			var data = Encoding.ASCII.GetBytes(postData);
-			request.Method = "POST";
-			request.ContentType = "application/x-www-form-urlencoded";
-			request.ContentLength = data.Length;
-			using (var stream = request.GetRequestStream())
-			{
-				stream.Write(data, 0, data.Length);
-			}
 
-			string responseData;
-			using (var response = (HttpWebResponse)request.GetResponse())
-				using (var dataStream = response.GetResponseStream())
-					using (var reader = new StreamReader(dataStream))
-						responseData = reader.ReadToEnd();
-			return responseData;
+			return PostRequest("get_account", postData);
 		}
 	}
 }
